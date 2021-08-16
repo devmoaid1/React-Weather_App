17e4e750e4491924c4182e9e651d173c64ef7586
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-08-17 03:49:32 +0800

    form container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import './App.css';
 import WeatherComponent from './components/weather';
 import FormContainer from './components/Form';
+
+export const API_KEY="e36ed364400282e43250b6c4c0274d44"
+//http://api.openweathermap.org/data/2.5/weather?q=cairo%2Cegypt&appid=e36ed364400282e43250b6c4c0274d44
 function App() {
 
-  const getWeather=(e)=>{
-    e.preventDefault()
-    console.log("submitted")
-  }
+ 
   return (
     <div className="App"> 
-    <FormContainer submit={getWeather} />
+    <FormContainer  />
     <WeatherComponent/>
       
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { API_KEY } from '../App'
+
 
 function FormContainer(props) {
 
-
-    const handleSubmit=props.submit
+    const [city,setCity]=useState("")
+    const [country,setCountry]=useState("")
+    
+    const[temprature,setTemprature]=useState("")
+    const[humidity,setHumidity]=useState("")
+    const[description,setDescription]=useState("")
+    
+    const getWeather=async(e)=>{
+        e.preventDefault()
+        console.log("submitted")
+        const api=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`)
+        const data=await api.json()
+        setTemprature(data.main.temp)
+        setHumidity(data.main.humidity)
+        setDescription(data.weather[0].description)
+        console.log(data,temprature)
+    }
+    const state={
+        city:city,
+        country:country,
+        temprature:temprature,
+        humidity:humidity,
+        description:description,
+        error:""
+    } 
+   
+    
     return (
-       <form  className="formcontainer"onSubmit={handleSubmit}>
-           <input type="text" placeholder="city......."></input>
-           <input type="text" placeholder="country....."></input>
+       <form  className="formcontainer"onSubmit={getWeather}>
+           <input type="text" name="city" onChange={(e)=>setCity(e.target.value)} placeholder="city......."></input>
+           <input type="text" name="country" onChange={(e)=>setCountry(e.target.value)}  placeholder="country....."></input>
            <button type="submit"> Get Weather</button>
            
        </form>
