1100336069c57950d09424180a5713869cdffd35
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-08-20 21:16:01 +0800

    code done

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,22 @@ import axios from 'axios'
 import WeatherComponent from './weather'
 function FormContainer() {
 
+    
+    
+    const [cityInput,setCityInput]=useState("")
+    const [countryInput,setCountryInput]=useState("")
+    
+    
     const [city,setCity]=useState("")
     const [country,setCountry]=useState("")
+
+
     const[error,setError]=useState("")
     const[temprature,setTemprature]=useState("")
     const[humidity,setHumidity]=useState("")
     const[description,setDescription]=useState("") 
-
-    const state={
-        
-        
+   
+    const state={  
         city:city,
         country:country,
         temprature:temprature,
@@ -26,16 +32,19 @@ function FormContainer() {
     
     const getWeather=async(e)=>{
         e.preventDefault()
-    if(city===""&country===""){setError("Please choose city and country")}
+    if(cityInput===""&&countryInput===""){setError("Please choose city and country")}
     else{
 
         console.log("submitted")
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=${API_KEY}`).then(res=>{
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}%2C${countryInput}&appid=${API_KEY}`).then(res=>{
 
             setTemprature(res.data.main.temp)
             setHumidity(res.data.main.humidity)
             setDescription(res.data.weather[0].description)
-
+            setCity(res.data.name)
+            setCountry(res.data.sys.country) 
+            
+            console.log(res.data)
 
         }).catch(err=>console.log(err))
         
@@ -45,21 +54,17 @@ function FormContainer() {
     
 }
 
-console.log(temprature,state)
    
     
     return (
-        <div>
-
-        
-       <form  className="formcontainer"onSubmit={getWeather}>
-           <input type="text" name="city" onChange={(e)=>setCity(e.target.value)} placeholder="city......."></input>
-           <input type="text" name="country" onChange={(e)=>setCountry(e.target.value)}  placeholder="country....."></input>
-           {error}
+        <div className="formcontainer">
+       <form onSubmit={getWeather}>
+           <input type="text" name="city" onChange={(e)=>setCityInput(e.target.value)} placeholder="city......."></input>
+           <input type="text" name="country" onChange={(e)=>setCountryInput(e.target.value)}  placeholder="country....."></input>
+           <p>{error}</p>
            <button type="submit"> Get Weather</button>
-       <WeatherComponent state={state}/>
        </form>
-       
+       <WeatherComponent state={state}/>
        </div>
     )
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -9,12 +9,33 @@ function WeatherComponent(props) {
     console.log(weatherDetails)
     return (
         <div className="weatherDetails">
-           <ui>
-               <div><li>location:</li></div>
-               <li>location:{weatherDetails.temprature}</li>
-               <li>location:</li>
-               <li>location:</li>
-           </ui>
+           
+            {  
+                weatherDetails.temprature &&  <p className="info">Temprature:<span className="info-value">{weatherDetails.temprature}</span></p>
+            }
+
+            { 
+               weatherDetails.country &&  <p className="info">Country:<span className="info-value">{weatherDetails.country}</span></p>
+
+            }
+
+            {
+                weatherDetails.city && <p className="info">City:<span className="info-value">{weatherDetails.city}</span></p>
+            }
+
+            {
+                weatherDetails.humidity &&  <p className="info">humidity:<span className="info-value">{weatherDetails.humidity}</span></p>
+            }
+
+            {
+                 weatherDetails.description && <p className="info">Description:<span className="info-value">{weatherDetails.description} </span></p>
+            }
+              
+              
+               
+              
+               
+           
         </div>
     )
 }
